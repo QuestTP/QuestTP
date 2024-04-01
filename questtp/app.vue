c13f1819534e1adb66b532d3de613bae84f7bb5b
Ryan Chen <ryanchen979@gmail.com>
2024-03-31 21:30:30 -0400

    initialize

---
+++ questtp/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout><NuxtPage /></NuxtLayout>
+  </div>
+</template>
+
+<style>
+body,
+html,
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  background-color: #021e1d;
+  color: #76b245;
+}
+</style>
---
+++ questtp/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Hello World</div>
+</template>
+
+<script></script>
+
+<style scoped></style>
